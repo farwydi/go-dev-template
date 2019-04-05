fa002f972aadf1f31cb0bb105685ce724dece508
Leonid Zharikov <leon.zharikov@gmail.com>
2019-04-05 10:25:22 +0300

    feat: add gin recover with zap log

---
+++ gin/recover.go
@@ -0,0 +1,45 @@
+package dev_tempalte_gin
+
+import (
+    "github.com/farwydi/go-dev-template"
+    "github.com/gin-gonic/gin"
+    "go.uber.org/zap"
+    "net"
+    "net/http"
+    "net/http/httputil"
+    "os"
+    "strings"
+)
+
+func Recovery(c *gin.Context) {
+    defer func() {
+        if err := recover(); err != nil {
+            // Check for a broken connection, as it is not really a
+            // condition that warrants a panic stack trace.
+            var brokenPipe bool
+            if ne, ok := err.(*net.OpError); ok {
+                if se, ok := ne.Err.(*os.SyscallError); ok {
+                    if strings.Contains(strings.ToLower(se.Error()), "broken pipe") || strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
+                        brokenPipe = true
+                    }
+                }
+            }
+
+            httpRequest, _ := httputil.DumpRequest(c.Request, false)
+            dev_tempalte.ZapLogger.Error("panic recovered",
+                zap.String("DumpRequest", string(httpRequest)),
+                zap.Reflect("err", err),
+            )
+
+            // If the connection is dead, we can't write a status to it.
+            if brokenPipe {
+                c.Error(err.(error)) // nolint: errcheck
+                c.Abort()
+            } else {
+                c.AbortWithStatus(http.StatusInternalServerError)
+            }
+        }
+    }()
+
+    c.Next()
+}
